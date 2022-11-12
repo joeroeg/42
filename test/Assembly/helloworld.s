18fe718475f777480380abae0e9c85f845bb71a9
joeroeg <6369633@gmail.com>
2022-11-11 20:22:44 -0500

    update

--- test/Assembly/helloworld.s
+++ test/Assembly/helloworld.s
@@ -1,13 +1,21 @@
-.text
-	.globl __start
-__start:
+; ----------------------------------------------------------------------------------------
+; Writes "Hello, World" to the console using only system calls. Runs on 64-bit Linux only.
+; To assemble and run:
+;
+;     nasm -fmacho64 hello.s && ld hello.o && ./a.out
+; ----------------------------------------------------------------------------------------
 
-		la $a0,str
-		li $v0,4
-		syscall
+          global    _main
 
-		li $v0,10
-		syscall
+          section   .text
+_main:   mov       rax, 0x2000004                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 0x2000001                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-		.data
-str:	.asciiz "hellow world\n"
+          section   .data
+message:  db        "Hello, World", 5      ; note the newline at the end
