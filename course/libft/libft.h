a30e29d7a273bf4779428b2c7e5ecf700ebdb86d
joe <6369633@gmail.com>
2022-12-19 18:10:49 -0500

    ft_putchar_fd

--- course/libft/libft.h
+++ course/libft/libft.h
@@ -20,6 +20,8 @@
 # include <stdint.h>
 # include <string.h>
 # include <limits.h>
+# include <sys/stat.h>
+# include <fcntl.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
@@ -30,6 +32,7 @@ size_t	ft_strlen(const char *s);
 void *ft_memset(void *str, int c, size_t n);
 void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void ft_putchar_fd(char c, int fd);
 //void ft_memmove(void *dst, const void *src, size_t n);
 //size_t ft_strlcpy(char *dst, const char *src, size_t size);
 //size_t ft_strlcat(char *dst, const char *src, size_t size);
@@ -51,7 +54,7 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 //char *ft_itoa(int n);
 //char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 //void ft_striteri(char *s, void (*f)(unsigned int, char*));
-//void ft_putchar_fd(char c, int fd);
+
 //void ft_putstr_fd(char *s, int fd);
 //void ft_putendl_fd(char *s, int fd);
 //void ft_putnbr_fd(int n, int fd);
--- course/libft/main.c
+++ course/libft/main.c
@@ -20,7 +20,8 @@ int	ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void ft_bzero(void *s, size_t n); 
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n); 
+void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void ft_putchar_fd(char c, int fd);
 //void ft_memmove(void *dst, const void *src, size_t n); 
 //size_t ft_strlcpy(char *dst, const char *src, size_t size); 
 //size_t ft_strlcat(char *dst, const char *src, size_t size); 
@@ -42,7 +43,6 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 //char *ft_itoa(int n);
 //char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 //void ft_striteri(char *s, void (*f)(unsigned int, char*));
-//void ft_putchar_fd(char c, int fd);
 //void ft_putstr_fd(char *s, int fd);
 //void ft_putendl_fd(char *s, int fd);
 //void ft_putnbr_fd(int n, int fd);
@@ -57,8 +57,7 @@ void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 // strcpy int	main(void){char str[50]; strcpy(str, "This is string.h library function"); puts(str); memset(str, '-', 7); puts(str); return (0); }
 // memset int	main (void){char str1[] = "Fuck hello ft_memset"; char str2[] = ".... hello memset"; ft_memset (str1, '-', 5); puts (str1); memset (str2, '-', 5); puts (str2); return (0); }
 // ft_bzero		int main(void){char str[] = "bzero"; ft_bzero(str, 4); printf("%s", str); return 0;} // я не понимаю почему значение 4 ничего не изменеяет
-// ft_memcpy	
-int main(void){char src[] = "memcpy"; char src2[] = "ft_memcpy"; char dst[7]; printf("Destination before memcpy: %s\n", dst); memcpy(dst, src, sizeof(src)); printf("Destination after memcpy: %s\n", dst); ft_bzero(dst, sizeof(dst)); printf("Destination before ft_memcpy: %s\n", dst); ft_memcpy(dst, src2, sizeof(src2)); printf("Destination after ft_memcpy: %s\n", dst); return 0;}
+// ft_memcpy	int main(void){char src[] = "memcpy"; char src2[] = "ft_memcpy"; char dst[7]; printf("Destination before memcpy: %s\n", dst); memcpy(dst, src, sizeof(src)); printf("Destination after memcpy: %s\n", dst); ft_bzero(dst, sizeof(dst)); printf("Destination before ft_memcpy: %s\n", dst); ft_memcpy(dst, src2, sizeof(src2)); printf("Destination after ft_memcpy: %s\n", dst); return 0;}
 // ft_memmove 
 // ft_strlcpy 
 // ft_strlcat 
@@ -81,6 +80,13 @@ int main(void){char src[] = "memcpy"; char src2[] = "ft_memcpy"; char dst[7]; pr
 // ft_strmapi 
 // ft_striteri  
 // ft_putchar_fd 
+int main(int argc, char **argv)
+{
+	int fd = open("myfile.txt", O_WRONLY | O_CREAT);
+	ft_putchar_fd('c', fd);
+	close(fd);
+	return 0;
+}
 // ft_putstr_fd 
 // ft_putendl_fd 
 // ft_putnbr_fd
