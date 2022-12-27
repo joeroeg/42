a9dddb61b6112dd573be4fa4e90bb9051db3c6b6
joe <6369633@gmail.com>
2022-12-27 05:26:05 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #42
+// ft_striteri - delete from main it's bonus
+ft_substr - crash
+ft_split - crash
+ft_itoa - failed
+ft_putendl_fd
+check forbidden functions
--- course/libft/srcs/ft_strjoin.c
+++ course/libft/srcs/ft_strjoin.c
@@ -21,7 +21,7 @@ char *ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	new_str = malloc(strlen(s1) + strlen(s2) + 1);
+	new_str = malloc(strlen(s1) + ft_strlen(s2) + 1);
 	if (new_str == NULL)
 		return (NULL);
 	i = 0;
